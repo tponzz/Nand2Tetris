88a5ac61bee6cdb6a329d09d78a85b62b3839ac0
tponzz <shi.na.telebla3@gmail.com>
2025-12-23 23:29:53 +0900

    Create VM translator without comparision

--- 7/vm/vm.cpp
+++ 7/vm/vm.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "code_writer.h"
+#include "parser.h"
+
 void
 Usage()
 {
@@ -16,11 +19,35 @@ main(int argc, char const* argv[])
         return -1;
     }
 
-    std::string input = argv[1];
+    std::string input  = argv[1];
     std::string output = argv[2];
 
     std::cout << "vm" << std::endl;
     std::cout << "in: " << input << std::endl;
     std::cout << "out: " << output << std::endl;
+
+    Vm::Parser p{ input };
+    Vm::CodeWriter writer{ output };
+
+    while (p.HasMoreLines()) {
+        p.Advance();
+
+        const auto type = p.CommandType();
+        switch (type) {
+            case Vm::Parser::Cmd::Pop:
+            case Vm::Parser::Cmd::Push: {
+                writer.WritePushPop(type, p.Arg1(), p.Arg2());
+            } break;
+            case Vm::Parser::Cmd::Arithmetic: {
+                writer.WriteArithmetic(p.Arg1());
+            } break;
+
+            default:
+                break;
+        }
+    }
+
+    std::cout << "Transration Finished" << std::endl;
+
     return 0;
 }
