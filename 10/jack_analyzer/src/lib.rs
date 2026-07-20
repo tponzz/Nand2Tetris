9e10431d56134317d2f6797d172f01caecb6ac45
tponzz <shi.na.telebla3@gmail.com>
2026-07-20 23:15:33 +0900

    Add temp main code
    - Output a token and its type

--- 10/jack_analyzer/src/lib.rs
+++ 10/jack_analyzer/src/lib.rs
@@ -3,6 +3,8 @@ mod tokenizer;
 
 use clap::Parser;
 use tokenizer::Tokenizer;
+
+use crate::tokenizer::TokenType;
 #[derive(Debug)]
 pub enum JAError {
     Io,
@@ -14,14 +16,40 @@ pub fn run() -> Result<(), JAError> {
 
     let mut tokenizer = Tokenizer::new(&cli.source);
     while tokenizer.has_more_tokens() {
-        tokenizer.advance();
+        if tokenizer.advance().is_err() {
+            return Err(JAError::Io);
+        };
 
-        //TODO
-        println!(
-            "keyword: {:?}, type: {:?}",
-            tokenizer.keyword(),
-            tokenizer.token_type()
-        );
+        if let Some(t) = tokenizer.token_type() {
+            match t {
+                TokenType::Keyword => {
+                    if let Some(key) = tokenizer.keyword() {
+                        println!("KEY: {:?}", key);
+                    }
+                }
+                TokenType::Symbol => {
+                    if let Some(sym) = tokenizer.symbol() {
+                        println!("SYM: {}", sym);
+                    }
+                }
+                TokenType::Identifier => {
+                    if let Some(id) = tokenizer.identifier() {
+                        println!("IDT: {}", id);
+                    }
+                }
+                TokenType::IntConst => {
+                    if let Some(val) = tokenizer.int_val() {
+                        println!("INT: {}", val);
+                    }
+                }
+                TokenType::StringConst => {
+                    if let Some(val) = tokenizer.string_val() {
+                        println!("STR: {}", val);
+                    }
+                }
+            }
+        };
     }
-    Err(JAError::Io)
+
+    Ok(())
 }
